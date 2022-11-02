8f59123a2435a5ed84ddc34289cf5cf39556967c
amanda vikdal <vikdal.amanda@gmail.com>
2022-11-01 18:41:03 -0600

    commit tree 69defd259792c0c54bdb87cc22aca87cd2ccbda3

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(i){
+
+  if (i <= 400){
+    return("This one is on me!");
+  } else if (i > 400 && i < 2000){
+    return("That will be twenty bucks.");
+  } else if (i > 2000 && i < 2500){
+    return("I will gladly take your thirty bucks.");
+  } else {
+    return("No can do.");
+  }
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC'?'Ok, sounds good.':'No go.' 
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case 'generous':
+      return'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
